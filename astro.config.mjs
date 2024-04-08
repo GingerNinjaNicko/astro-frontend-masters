db92e67284452e80550a66a87bc623b0def208ee
Nick Ruddock <Nick.Ruddock@taxassist.co.uk>
2024-04-08 10:33:18 +0100

    🔧 🐛 fix multiple framework integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
-import solidJs from "@astrojs/solid-js";
+import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), solidJs()]
+  integrations: [
+    react({
+      include: ['./src/components/react/*'],
+    }),
+    solid({
+      include: ['./src/components/solid/*'],
+    }),
+  ]
 });
