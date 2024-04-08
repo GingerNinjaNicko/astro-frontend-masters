33e8dee8f0ab25ce71d52d5e19e50b39cb230fbe
Nick Ruddock <Nick.Ruddock@taxassist.co.uk>
2024-04-08 15:51:36 +0100

    ✨ RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astro-frontend-masters-nicko.netlify.app',
   integrations: [
     react({
       include: ['./src/components/react/*'],
