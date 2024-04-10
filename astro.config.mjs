6151fe3a79cb9f3d387fe33f26dd34463dafda2d
Nick Ruddock <Nick.Ruddock@taxassist.co.uk>
2024-04-10 11:21:40 +0100

    Site ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import solid from "@astrojs/solid-js";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-frontend-masters-nicko.netlify.app',
-  integrations: [
-    react({
-      include: ['./src/components/react/*'],
-    }),
-    solid({
-      include: ['./src/components/solid/*'],
-    }),
-  ]
+  output: 'hybrid',
+  integrations: [react({
+    include: ['./src/components/react/*']
+  }), solid({
+    include: ['./src/components/solid/*']
+  })],
+  adapter: netlify()
 });
